d8426e99491aff1ff25ed21762a5b0db796eda41
ابراهيم السيد سليم محمد عبد العال <IBRAHIEM.Selem291@el-eng.menofia.edu.eg>
2020-10-30 16:53:00 +0200

    Adding java beans.

---
+++ src/Student.java
@@ -0,0 +1,24 @@
+import java.io.Serializable;
+
+public class Student implements Serializable{
+	
+	private String name;
+	private int id;
+	public Student() {
+		
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/StudentInfo.java
@@ -0,0 +1,10 @@
+
+public class StudentInfo {
+	public static void main(String[] args) {
+		Student st = new Student();
+		st.setId(500);
+		st.setName("ibrahim");
+		System.out.println("id: "+st.getId());
+		System.out.println("Name: "+st.getName());
+	}
+}
